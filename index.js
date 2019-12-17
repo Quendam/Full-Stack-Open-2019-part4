8a0438e6d70dc45b2512edd880a75ea06f54e98c
Petri Kataja <peejkoo@gmail.com>
2019-12-17 21:07:31 +0200

    4.2 Moved routes to their own controllers folder

--- index.js
+++ index.js
@@ -3,28 +3,12 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const Blog = require('./models/blog');
+const blogsRouter = require('./controllers/blog');
 
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
