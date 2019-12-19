ba9f6de012da12c6c5929d14670ddb6cdba65253
Petri Kataja <peejkoo@gmail.com>
2019-12-19 20:05:11 +0200

    4.5 Added favorite blogs and tests for it

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,7 +4,22 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => blogs.reduce((prev, cur) => prev + cur.likes, 0);
 
+const favoriteBlog = (blogs) => {
+  let favorite = {
+    likes: -1
+  };
+
+  blogs.forEach(current => {
+    if(current.likes > favorite.likes) {
+      favorite = current
+    }
+  });
+
+  return favorite.likes == -1 ? {} : favorite;
+}
+
 module.exports = {
   dummy,
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
