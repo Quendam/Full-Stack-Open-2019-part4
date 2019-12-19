202ac7e5fdd4eac89f0eb98beb30f75c544f19cb
Petri Kataja <peejkoo@gmail.com>
2019-12-19 19:41:56 +0200

    4.4 Added total likes and some tests for it

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -6,3 +6,59 @@ test('dummy returns one', () => {
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
 })
+
+describe('total likes', ()=> {
+
+  test('of empty is zero', () =>{
+    expect(listHelper.totalLikes([])).toBe(0);
+  })
+
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+      likes: 5,
+      __v: 0
+    }
+  ]
+
+  test('when list has only one blog equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
+
+  const listOfBlogs = [
+    {
+      _id: '1',
+      title: 'Blog1',
+      author: 'Petri Kataja',
+      url: 'http://www.mooc.fi/#blog1',
+      likes: 5,
+      __v: 0
+    },
+    {
+      _id: '2',
+      title: 'Blog 2',
+      author: 'Petri Kataja',
+      url: 'http://www.mooc.fi/#blog2',
+      likes: 15,
+      __v: 0
+    },
+    {
+      _id: '3',
+      title: 'Blog 3',
+      author: 'Petri Kataja',
+      url: 'http://www.mooc.fi/#blog3',
+      likes: 50,
+      __v: 0
+    }
+  ]
+
+  test('of bigger list is calculated right', () => {
+    const result = listHelper.totalLikes(listOfBlogs)
+    expect(result).toBe(70)
+  })
+
+});
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,10 @@
 const dummy = (blogs) => {
-    return 1
-  }
-  
-  module.exports = {
-    dummy
-  }
+  return 1
+}
+
+const totalLikes = (blogs) => blogs.reduce((prev, cur) => prev + cur.likes, 0);
+
+module.exports = {
+  dummy,
+  totalLikes
+}
