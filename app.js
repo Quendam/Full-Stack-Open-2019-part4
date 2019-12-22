48069a3be5cf33b26bd79133ac2c654321b63480
Petri Kataja <peejkoo@gmail.com>
2019-12-22 17:40:00 +0200

    4.8 Added first backend tests for databaseAlso added cros-env and logger

--- app.js
+++ app.js
@@ -3,17 +3,18 @@ const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const app = express()
 const cors = require('cors')
-const blogsRouter = require('./controllers/blog');
+const blogsRouter = require('./controllers/blog')
 const config = require('./utils/config')
+const logger = require('./utils/logger')
 
-console.log('connecting to', config.MONGODB_URI)
+logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log('connected to MongoDB')
+    logger.info('connected to MongoDB')
   })
   .catch((error) => {
-    console.log('error connecting to MongoDB:', error.message)
+    logger.info('error connecting to MongoDB:', error.message)
   })
 
 app.use(cors())
