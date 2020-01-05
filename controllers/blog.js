c35cf384aff0d225187089306b1ff3da9fae0312
Petri Kataja <peejkoo@gmail.com>
2020-01-05 11:38:32 +0200

    7.11 Added ability to add comments to blog

--- controllers/blog.js
+++ controllers/blog.js
@@ -44,6 +44,7 @@ blogsRouter.post('/', async (request, response, next) => {
       author: body.author,
       url: body.url,
       likes: body.likes,
+      comments: body.comments,
       user: user._id
     })
 
@@ -95,6 +96,7 @@ blogsRouter.put('/:id', async (request, response) => {
     author: body.author,
     url: body.url,
     likes: body.likes,
+    comments: body.comments,
     user: user._id
   }
 
@@ -129,6 +131,35 @@ blogsRouter.delete('/:id', async (request, response, next) => {
     await Blog.findByIdAndDelete(request.params.id)
 
     response.status(204).end()
+  }catch(exception){
+    next(exception)
+  }
+
+})
+
+blogsRouter.post('/:id/comments', async (request, response, next) => {
+  try{    
+    const id = request.params.id
+    const body = request.body
+
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    
+    if(!body.comment){
+      return response.status(401).json({
+        error: 'comment is missing'
+      })
+    }
+
+    const blog = await Blog.findById(id)
+
+    blog.comments.push(body.comment)
+    
+    await Blog.findByIdAndUpdate(request.params.id, blog)
+    
+    response.status(201).end()
   }catch(exception){
     next(exception)
   }
